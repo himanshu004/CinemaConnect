bb887117657ebbd4424b5561ed36a7de0d49651e
Himanshu Sharma <himanshusharma39692@gmail.com>
2025-09-05 20:02:44 +0530

    Fix the server issue

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -78,8 +78,7 @@ userSchema.pre('save', async function(next) {
   if (!this.isModified('password')) return next();
   
   try {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
+    this.password,
     next();
   } catch (error) {
     next(error);
@@ -88,7 +87,7 @@ userSchema.pre('save', async function(next) {
 
 // Method to compare passwords
 userSchema.methods.comparePassword = async function(candidatePassword) {
-  return bcrypt.compare(candidatePassword, this.password);
+  return await bcrypt.compare(candidatePassword, this.password);
 };
 
 const User = mongoose.model('User', userSchema);
